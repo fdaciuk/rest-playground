99bc39239886e1eb4f3b4a9fc17e276ec09abf51
Fernando Daciuk <f.daciuk@gmail.com>
2015-08-03 07:23:06 -0300

    Add crud with levelDB

--- nodejs/contacts-list/levelup.js
+++ nodejs/contacts-list/levelup.js
@@ -56,6 +56,59 @@ app.get('/contacts/:number', function(request, response) {
   });
 });
 
+app.post('/contacts/:number', function(request, response) {
+  console.log( 'Adding new contact with primary number', request.params.number );
+  console.log('body', request.body);
+  db.put(request.params.number, request.body, function(error) {
+    if(error) {
+      response.writeHead(500, {
+        'Content-Type': 'text-plain'
+      });
+      response.end('Internal Server Error');
+      return;
+    }
+    response.send(request.params.number + ' succesfully inserted');
+  });
+});
+
+app.del('/contacts/:number', function(request, response) {
+  console.log( 'Deleting contact with primary number', request.params.name );
+  db.del(request.params.number, function(error) {
+    if(error) {
+      response.writeHead(500, {
+        'Content-Type': 'text/plain'
+      });
+      response.end('Internal Server Error');
+      return;
+    }
+    response.send(request.params.number + ' succesfully deleted');
+  });
+});
+
+app.get('/contacts', function(request, response) {
+  console.log('Listing all contacts');
+  var isFirst = true;
+  response.setHeader('content-type', 'application/json');
+  db.createReadStream()
+  .on('data', function(data) {
+    console.log('Readable stream', data.value);
+    if(isFirst) response.write('[');
+    else response.write(',');
+    response.write(JSON.stringify(data.value));
+    isFirst = false;
+  })
+  .on('error', function(error) {
+    console.log('Error while reading', error);
+  })
+  .on('close', function() {
+    console.log('Closing db stream');
+  })
+  .on('end', function() {
+    console.log('Db stream closed');
+    response.end(']');
+  });
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
